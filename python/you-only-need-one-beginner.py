eebece787f26e2e3f641cee42bcad09b0f24430a
Shawn <134296052+shawnvera@users.noreply.github.com>
2023-09-23 11:06:42 -0400

    added kata

---
+++ python/you-only-need-one-beginner.py
@@ -0,0 +1,10 @@
+# Begin
+# Input is an array
+# Input is also a value
+# Array can contain numbers or strings
+# check to see if the value is in the array
+# if in the array return true else return false
+# End
+
+def check(seq, elem):
+    return elem in seq
