58d9b831168669694c2dca045d8ea235894b460e
Shawn <134296052+shawnvera@users.noreply.github.com>
2023-09-25 17:42:48 -0400

    added kata

---
+++ python/areyouplayingbanjo.py
@@ -0,0 +1,8 @@
+def are_you_playing_banjo(name):
+    # Conditional if first index of name is equal to "r" elif "R" else
+    if (name[0] == "r"):
+        return name + " plays banjo"
+    elif (name[0] == "R"):
+        return name + " plays banjo"
+    else:
+        return name + " does not play banjo"
